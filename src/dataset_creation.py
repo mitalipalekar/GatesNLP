96f10321b094a2fe7bff39502980c2b02f7f7111
Swojit Mohapatra <neelmohapatra@gmail.com>
2019-04-23 22:56:22 -0700

    Added filter by venue, but code breaks, needs fixing

--- src/dataset_creation.py
+++ src/dataset_creation.py
@@ -3,9 +3,12 @@ import wget
 import os
 import gzip
 import glob
+import json
 
 
 BASE_URL = 'https://s3-us-west-2.amazonaws.com/ai2-s2-research-public/open-corpus/'
+OUTPUT = 'dataset_final'
+
 def get_corpus_files(filename):
     return [line.strip() for line in filename if line.startswith('corpus')]
 
@@ -14,17 +17,23 @@ def main():
     manifest_file = open('manifest.txt', "r")
     corpus_files = get_corpus_files(manifest_file)
     print(corpus_files)
-    for corpus_file in corpus_files[:1]:
-	    wget.download(BASE_URL + corpus_file)
-	    name = corpus_file[corpus_file.index('/') + 1:]
-	    with gzip.open(name) as f:
-		    for line in f:
-			    print(line)
+    with open(OUTPUT, 'a+') as out:
+	    # REads one file for now, fix bugs and then do all
+	    for corpus_file in corpus_files[:1]:
+		    wget.download(BASE_URL + corpus_file)
+		    name = corpus_file[corpus_file.index('/') + 1:]
+		    with gzip.open(name) as f:
+			    for line in f:
+				    parsed_json = json.loads(line)
+				    # Breaks here
+				    if line['venue'] in ['ACL', 'NAACL','EMNLP']:
+					    out.write(line + '\n')
+							
 
-            # remove files after done
-	    for f in glob.glob("s2*"):
-		    os.remove(f)
-	    
+		    # remove files after done
+		    for f in glob.glob("s2*"):
+			    os.remove(f)
+		    
 
 
 
