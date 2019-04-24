79141edbd0cd357864b2fdac9912904cc0268c26
Mitali Palekar <mitali97@cs.washington.edu>
2019-04-24 00:39:58 -0700

    Fixed bugs in dataset_creation

--- src/dataset_creation.py
+++ src/dataset_creation.py
@@ -16,22 +16,25 @@ def get_corpus_files(filename):
 def main():
     manifest_file = open('manifest.txt', "r")
     corpus_files = get_corpus_files(manifest_file)
-    print(corpus_files)
     with open(OUTPUT, 'a+') as out:
-	    # REads one file for now, fix bugs and then do all
-	    for corpus_file in corpus_files[:1]:
-		    wget.download(BASE_URL + corpus_file)
-		    name = corpus_file[corpus_file.index('/') + 1:]
-		    with gzip.open(name) as f:
-			    for line in f:
-				    parsed_json = json.loads(line)
-				    if parsed_json['venue'] in ['ACL', 'NAACL','EMNLP']:
-					    out.write(line + '\n')
+        # Reads one file for now, fix bugs and then do all
+        for corpus_file in corpus_files:
+            wget.download(BASE_URL + corpus_file)
+            name = corpus_file[corpus_file.index('/') + 1:]
+            with gzip.open(name) as f:
+                i = 0
+                for line in f:
+                    if i % 500 == 0:
+                        print(str(i) + ' processed')
+                    i = i + 1
+                    parsed_json = json.loads(line)
+                    if parsed_json['venue'] in ['ACL', 'NAACL','EMNLP']:
+                        out.write(line.decode())
 							
 
-		    # remove files after done
-		    for f in glob.glob("s2*"):
-			    os.remove(f)
+            # remove files after done
+            for f in glob.glob("s2*"):
+                os.remove(f)
 		    
 
 
