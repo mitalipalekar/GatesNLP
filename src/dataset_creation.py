3757ab06c18c3cd6e03133e2e27f35bd0b42a086
Swojit Mohapatra <neelmohapatra@gmail.com>
2019-04-23 22:40:21 -0700

    Can unzip and remove each blob

--- src/dataset_creation.py
+++ src/dataset_creation.py
@@ -1,17 +1,32 @@
 #! /usr/bin/env python3
+import wget
+import os
+import gzip
+import glob
 
+
+BASE_URL = 'https://s3-us-west-2.amazonaws.com/ai2-s2-research-public/open-corpus/'
 def get_corpus_files(filename):
-    files = []
-    for line in filename:
-        print(line)
-        if line.startswith('corpus'):
-            files.append(line.rstrip("\n"))
-    return files
+    return [line.strip() for line in filename if line.startswith('corpus')]
+
 
 def main():
     manifest_file = open('manifest.txt', "r")
     corpus_files = get_corpus_files(manifest_file)
     print(corpus_files)
+    for corpus_file in corpus_files[:1]:
+	    wget.download(BASE_URL + corpus_file)
+	    name = corpus_file[corpus_file.index('/') + 1:]
+	    with gzip.open(name) as f:
+		    for line in f:
+			    print(line)
+
+            # remove files after done
+	    for f in glob.glob("s2*"):
+		    os.remove(f)
+	    
+
+
 
 if __name__ == '__main__':
     main()
