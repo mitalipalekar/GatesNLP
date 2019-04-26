f5741798157e18e3ea337926b8c257dc820d6df5
Bryan Hanner <bryhan@cs.washington.edu>
2019-04-25 18:57:44 -0700

    adds data split based on time, flags for our experiments, and error analysis prints

--- src/strawman.py
+++ src/strawman.py
@@ -10,6 +10,9 @@ from tqdm import tqdm
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from scipy import spatial
+import math
+
+from pprint import pprint
 
 from numpy import dot
 from numpy.linalg import norm
@@ -19,14 +22,10 @@ PAPERS: str = "dataset_final"
 
 
 def tf_idf_ranking():
-    nlp = spacy.load("en_core_web_sm")
-    tokenizer = English().Defaults.create_tokenizer(nlp)
     vectorizer = TfidfVectorizer()
     abstracts = []
-    count = 0
     with open(PAPERS, 'rb') as f:
         for line in tqdm(f):
-            count += 1
             parsed_json = json.loads(line)
             abstracts.append(parsed_json['paperAbstract'])
 
@@ -37,40 +36,65 @@ def main():
     nlp = spacy.load("en_core_web_sm")
     tokenizer = English().Defaults.create_tokenizer(nlp)
 
-    ids = []
-    abstracts = []
-    titles = []
-    out_citations = []
+    is_jaccard = len(sys.argv) > 1 and sys.argv[1] == "j"
+    lemmatize = len(sys.argv) > 2 and sys.argv[2] == "1"
+
+    lines = []
     with open(PAPERS, 'rb') as f:
-        for line in tqdm(f):
-            ids.append(json.loads(line)['id'])
-            abstracts.append(json.loads(line)['paperAbstract'])
-            titles.append(json.loads(line)['title'])
-            out_citations.append(json.loads(line)['outCitations'])
+        for line in f:
+            lines.append(json.loads(line))
+
+    lines.sort(key=lambda x: x['year'])
+
+    ids = extract_keys(lines, 'id')
+    abstracts = extract_keys(lines, 'paperAbstract')
+    titles = extract_keys(lines, 'title')
+    out_citations = extract_keys(lines, 'outCitations')
 
     train_ids, dev_ids, test_ids = split_data(ids, 0.8, 0.9)
     train_abstracts, dev_abstracts, test_abstracts = split_data(abstracts, 0.8, 0.9)
     train_title, dev_title, test_title = split_data(titles, 0.8, 0.9)
     train_out_citations, dev_out_citations, test_out_citations = split_data(out_citations, 0.8, 0.9)
 
+    print("train size = " + str(len(train_ids)))
+    print("dev size = " + str(len(dev_ids)))
+
     # gets the tokens of the train
-    train_token_rows = [set(get_tokens(tokenizer, paper)) for paper in train_abstracts]
+    train_token_rows = [set(get_tokens(tokenizer, paper, lemmatize)) for paper in train_abstracts]
+
+    total_count = 0
+    citation_counts = dict()
+    for i, citations in enumerate(dev_out_citations):
+        count = len(set(train_ids) & set(citations))
+        total_count += count
+        citation_counts[i] = count
+    print("total count = " + str(total_count))
+    print(dev_title[527])
+    pprint(sorted(citation_counts.items(), key = lambda kv:(kv[1], kv[0])))
+    print(set(split_data(extract_keys(lines, 'year'), 0.8, 0.9)[0]))
 
     # get file to write titles too
-    f = open("titles_similar_dataset_final.txt", "w")
+    f = open("titles_similar_dataset_final.txt", "w", encoding="utf-8")
     f.write("test title, top-10 similar papers\n")
 
     # evaluation metric
     tfidf_matrix = tf_idf_ranking()
     eval_score = []
+    matching_citation_count = 0
+    max_rank = float("-inf")
     for i, dev_row in tqdm(enumerate(dev_abstracts)):
         rankings = []
-        # dev_tokens = set(get_tokens(tokenizer, dev_row))
+        dev_tokens = set(get_tokens(tokenizer, dev_row, lemmatize))
         # get jaccard similarity for all the papers
         for index, train_tokens in enumerate(train_token_rows):
             a = tfidf_matrix[i + len(train_token_rows)]
             b = tfidf_matrix[index]
-            rankings.append((dot(a, b)/(norm(a)*norm(b)), index))
+
+            if is_jaccard:
+                score = jaccard_similarity(dev_tokens, train_tokens)
+            else:
+                score = dot(a, b)/(norm(a)*norm(b))
+            rankings.append((score, index))
         rankings.sort(key=lambda x: x[0], reverse=True)
 
         # EVALUATION METRIC LOGIC
@@ -79,16 +103,31 @@ def main():
         if len(out_citations):
             # gets the rankings of the training papers in the correct order
             ranking_ids = get_ids(rankings, train_ids)
-            find_citation = next((out_citation for out_citation in out_citations if out_citation in ranking_ids), None)
-            if find_citation:
-                eval_score.append(1.0 / (ranking_ids.index(find_citation) + 1))
+            list_citations = [out_citation for out_citation in out_citations if out_citation in ranking_ids]
+
+            if len(list_citations):
+                matching_citation_count += 1
+                rank = ranking_ids.index(list_citations[0]) + 1
+                max_rank = max(max_rank, rank)
+                eval_score.append(1.0 / rank)
+            correct_rankings = list(filter(lambda x: x in train_ids, list_citations))
+            print("correct abstracts")
+            for j in range(3):
+                if j < len(correct_rankings):
+                    print(train_abstracts[train_ids.index(correct_rankings[j])])
 
             # PRINT TOP 10 TITLES PER TEST PAPER
             paper_titles = get_relevant_papers(rankings[:10], train_title)
             f.write(test_title[i] + "\n " + ','.join(list(paper_titles)) + "\n\n")
+    print("matching citation count = " + str(matching_citation_count))
     print(eval_score)
+    print("max rank = " + str(max_rank))
     print(sum(eval_score) / float(len(test_ids)))
     f.close()
+
+
+def extract_keys(lines, key: str):
+    return [json[key] for json in lines]
 
 
 def jaccard_similarity(a, b):
@@ -98,9 +137,11 @@ def jaccard_similarity(a, b):
     return len(c) / (len(a) + len(b) - len(c))
 
 
-def get_tokens(tokenizer, paper: str) -> Set[str]:
-    tokens = tokenizer(paper.lower())
-    return {token.lemma_ for token in tokens if token.is_alpha and not token.is_stop}
+def get_tokens(tokenizer, paper: str, lemmatize: bool) -> Set[str]:
+    if lemmatize:
+        tokens = tokenizer(paper.lower())
+        return {token.lemma_ for token in tokens if token.is_alpha and not token.is_stop}
+    return paper.split()
 
 
 def get_relevant_papers(rankings, train_title):
