6dead4d0c80e1ea8a6968f237cecbf16a6b94540
Mitali Palekar <mitali97@cs.washington.edu>
2019-04-29 12:46:04 -0700

    Added in security conferences

--- src/dataset_creation.py
+++ src/dataset_creation.py
@@ -7,8 +7,8 @@ import json
 from tqdm import tqdm
 
 BASE_URL = 'https://s3-us-west-2.amazonaws.com/ai2-s2-research-public/open-corpus/'
-OUTPUT = 'temp'
-VENUES = ['ACL', 'NAACL', 'EMNLP']
+OUTPUT = 'extended_dataset.txt'
+VENUES = ['ACL', 'NAACL', 'EMNLP', 'ACM Conference on Computer and Communications Security', 'IEEE Symposium on Security and Privacy', 'IEEE International Conference on Information Theory and Information Security', 'IEEE Transactions on Information Forensics and Security', 'USENIX Security Symposium']
 
 
 def main():
@@ -30,12 +30,18 @@ def process_corpus(name, out):
     with gzip.open(name) as f:
         for line in tqdm(f, desc='Line'):
             parsed_json = json.loads(line)
-            if parsed_json['venue'] in VENUES:
+            if substring_of_array(parsed_json['venue']):
                 out.write(line.decode())
 
 
 def get_corpus_files(filename):
     return [line.strip() for line in filename if line.startswith('corpus')]
+
+def substring_of_array(parsed_venue):
+    for venue in VENUES:
+        if venue in parsed_venue:
+            return True;
+    return False
 
 
 if __name__ == '__main__':
