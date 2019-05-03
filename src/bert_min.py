6f1df687a576138a8710e51bdc0e86c129567970
Swojit Mohapatra <neelmohapatra@gmail.com>
2019-05-02 23:24:32 -0700

    mrr

--- src/bert_min.py
+++ src/bert_min.py
@@ -2,6 +2,10 @@ import json
 import sys
 import torch
 from pytorch_pretrained_bert import BertTokenizer, BertModel
+from numpy import dot
+from numpy.linalg import norm
+from tqdm import tqdm
+
 
 def bert(abstract):
     # Load pre-trained model tokenizer (vocabulary)
@@ -34,7 +38,7 @@ def bert(abstract):
 def generate_word_embeddings(papers):
     lines = []
     with open(papers, 'rb') as f:
-        for line in f:
+        for line in tqdm(f):
             lines.append(json.loads(line))
 
     lines.sort(key=lambda x: x['year'])
@@ -52,21 +56,41 @@ def generate_word_embeddings(papers):
     train_title, eval_title = split_data(titles, 0.8, 0.9, is_test)
     train_out_citations, eval_out_citations = split_data(out_citations, 0.8, 0.9, is_test)
 
+    eval_score = []
+    matching_citation_count = 0
+    min_rank = float("inf")
     word_embeddings_train = []
-    for abstract in train_abstracts:
+    for abstract in tqdm(train_abstracts):
         word_embedding = bert(abstract)
         word_embeddings_train.append(word_embedding)
 
-    for abstract in eval_abstracts:
+    for i, abstract in tqdm(enumerate(eval_abstracts)):
         word_embedding_eval = bert(abstract)
         rankings = []
         for train_index, word_embedding_train in enumerate(word_embeddings_train):
-            score = dot(word_embedding_eval, word_embedding_train) / (norm(word_embedding_eval)*norm(word_embedding_train))
+            score = dot(word_embedding_eval, word_embedding_train) / (
+                    norm(word_embedding_eval) * norm(word_embedding_train))
             rankings.append((score, train_index))
 
         rankings.sort(key=lambda x: x[0], reverse=True)
-        
+
         # TODO: now do MRR
+        out_citations = eval_out_citations[i]
+        if len(out_citations):
+            # gets the rankings of the training papers in the correct order
+            ranking_ids = get_ids(rankings, train_ids)
+            true_citations = [citation for citation in ranking_ids if citation in out_citations]
+
+            if len(true_citations):
+                matching_citation_count += 1
+                rank = ranking_ids.index(true_citations[0]) + 1
+                min_rank = min(min_rank, rank)
+                eval_score.append(1.0 / rank)
+
+    print("matching citation count = " + str(matching_citation_count))
+    print(eval_score)
+    print("min rank = " + str(min_rank))
+    print(sum(eval_score) / matching_citation_count)
 
 
 def split_data(data, dev_start: float, test_start: float, is_test: bool):
@@ -77,6 +101,11 @@ def split_data(data, dev_start: float, test_start: float, is_test: bool):
 
 def extract_keys(lines, key: str):
     return [json[key] for json in lines]
+
+
+def get_ids(rankings, train_ids):
+    return [train_ids[index] for _, index in rankings]
+
 
 def main():
     if len(sys.argv) < 1:
