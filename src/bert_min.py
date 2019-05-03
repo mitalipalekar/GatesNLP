3325345125ba16bf4dc6c2773167f4b7de453cf3
Mitali Palekar <mitali97@cs.washington.edu>
2019-05-02 22:11:50 -0700

    Implemented cosine similarity

--- src/bert_min.py
+++ src/bert_min.py
@@ -9,14 +9,11 @@ def bert(abstract):
 
     # Tokenized input
     tokenized_text = tokenizer.tokenize(abstract)
-    print(tokenized_text)
 
     # Convert token to vocabulary indices
     indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
     # Convert inputs to PyTorch tensors
     tokens_tensor = torch.tensor([indexed_tokens])
-
-    print(tokens_tensor)
 
     # Load pre-trained model (weights)
     model = BertModel.from_pretrained('bert-base-uncased')
@@ -55,10 +52,21 @@ def generate_word_embeddings(papers):
     train_title, eval_title = split_data(titles, 0.8, 0.9, is_test)
     train_out_citations, eval_out_citations = split_data(out_citations, 0.8, 0.9, is_test)
 
-    word_embeddings = []
-    for abstract in eval_abstracts:
+    word_embeddings_train = []
+    for abstract in train_abstracts:
         word_embedding = bert(abstract)
-        word_embeddings.append(word_embedding)
+        word_embeddings_train.append(word_embedding)
+
+    for abstract in eval_abstracts:
+        word_embedding_eval = bert(abstract)
+        rankings = []
+        for train_index, word_embedding_train in enumerate(word_embeddings_train):
+            score = dot(word_embedding_eval, word_embedding_train) / (norm(word_embedding_eval)*norm(word_embedding_train))
+            rankings.append((score, train_index))
+
+        rankings.sort(key=lambda x: x[0], reverse=True)
+        
+        # TODO: now do MRR
 
 
 def split_data(data, dev_start: float, test_start: float, is_test: bool):
