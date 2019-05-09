0136e856d865a98e984daa3076ab1e56dde68dc9
Swojit Mohapatra <neelmohapatra@gmail.com>
2019-05-08 17:05:00 -0700

    Bert fix

--- src/bert_embeddings.py
+++ src/bert_embeddings.py
@@ -44,7 +44,38 @@ def bert(abstract):
     with torch.no_grad():
         encoded_layers, _ = model(tokens_tensor)
     # We have a hidden states for each of the 12 layers in model bert-base-uncased
-    return encoded_layers[11end(word_embedding)
+    return encoded_layers[11]
+
+
+def generate_word_embeddings(papers):
+    lines = []
+    with open(papers, 'rb') as f:
+        for line in tqdm(f):
+            lines.append(json.loads(line))
+
+    lines.sort(key=lambda x: x['year'])
+
+    ids = extract_keys(lines, 'id')
+    abstracts = extract_keys(lines, 'paperAbstract')
+    titles = extract_keys(lines, 'title')
+    out_citations = extract_keys(lines, 'outCitations')
+
+    # TODO: DO NOT HARDCODE THIS
+    is_test = False
+
+    train_ids, eval_ids = split_data(ids, 0.8, 0.9, is_test)
+    train_abstracts, eval_abstracts = split_data(abstracts, 0.8, 0.9, is_test)
+    train_title, eval_title = split_data(titles, 0.8, 0.9, is_test)
+    train_out_citations, eval_out_citations = split_data(out_citations, 0.8, 0.9, is_test)
+
+    eval_score = []
+    matching_citation_count = 1
+    min_rank = float("inf")
+    word_embeddings_train = []
+    for abstract in tqdm(train_abstracts, desc='extracting embeddings for training set'):
+        if abstract:
+            word_embedding = take_mean_bert(bert(abstract))
+            word_embeddings_train.append(word_embedding)
     with open(WORD_EMBEDDINGS_TRAIN, 'wb') as handle:
         pickle.dump(word_embeddings_train, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
