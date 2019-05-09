b408037074f9547442f4d74b47020df932fc54d6
Mitali Palekar <mitali97@cs.washington.edu>
2019-05-08 18:39:46 -0700

    Imlemented cosine similarity + MRR for Doc2Vec

--- src/doc2vec_embeddings.py
+++ src/doc2vec_embeddings.py
@@ -5,7 +5,7 @@ from gensim.models import Doc2Vec
 from gensim.models.doc2vec import TaggedDocument
 from tqdm import tqdm, trange
 
-from gnlputils import extract_keys, split_data
+from gnlputils import extract_keys, split_data, get_from_rankings
 
 WORD_EMBEDDINGS_EVAL = 'doc2vec_eval.pk'
 WORD_EMBEDDINGS_TRAIN = 'doc2vec_train.pk'
@@ -20,9 +20,17 @@ def generate_word_embeddings(papers):
 
     lines.sort(key=lambda x: x['year'])
 
+    ids = extract_keys(lines, 'id')
     abstracts = extract_keys(lines, 'paperAbstract')
+    out_citations = extract_keys(lines, 'outCitations')
 
-    train_abstracts, eval_abstracts = split_data(abstracts, 0.8, 0.9, False)
+    # TODO: DO NOT HARDCODE THIS
+    is_test = False
+
+    train_ids, eval_ids = split_data(ids, 0.8, 0.9, is_test)
+    train_abstracts, eval_abstracts = split_data(abstracts, 0.8, 0.9, is_test)
+    train_out_citations, eval_out_citations = split_data(out_citations, 0.8, 0.9, is_test)
+
     dictionary = unk_train(train_abstracts)
     train_docs = create_tagged_doc(train_abstracts, dictionary)
 
@@ -31,12 +39,37 @@ def generate_word_embeddings(papers):
 
     for _ in trange(1): # TODO - need to change to 20 epochs
         model.train(train_docs, epochs=model.iter, total_examples=model.corpus_count)
-    # TODO - only for debugging purposes, please delete later
-    print(model.most_similar('embeddings'))
 
     # NOTE: Make sure to always UNK everything!
-    document_similarity = model.n_similarity(unk_abstract(train_abstracts[0].split(), dictionary), unk_abstract(eval_abstracts[1].split(), dictionary))
-    print(document_similarity)
+    eval_score = []
+    matching_citation_count = 1
+    min_rank = float("inf")
+    for i, eval_abstract in tqdm(enumerate(eval_abstracts), desc='generating rankings for evaluation set'):
+        rankings = []
+        for j, train_abstract in enumerate(train_abstracts[:10]):
+            if len(eval_abstract.split()) and len(train_abstract.split()):
+                document_similarity = model.n_similarity(unk_abstract(train_abstract.split(), dictionary),
+                                                         unk_abstract(eval_abstract.split(), dictionary))
+            rankings.append((document_similarity, j))
+        rankings.sort(key=lambda x: x[0], reverse=True)
+
+        out_citations = eval_out_citations[i]
+        if len(out_citations):
+            # gets the rankings of the training papers in the correct order
+            ranking_ids = get_from_rankings(rankings, train_ids)
+            true_citations = [citation for citation in ranking_ids if citation in out_citations]
+
+            if len(true_citations):
+                matching_citation_count += 1
+                rank = ranking_ids.index(true_citations[0]) + 1
+                min_rank = min(min_rank, rank)
+                eval_score.append(1.0 / rank)
+        break
+
+    print("matching citation count = " + str(matching_citation_count))
+    print(eval_score)
+    print("min rank = " + str(min_rank))
+    print(sum(eval_score) / matching_citation_count)
 
 
 def create_tagged_doc(abstracts: [str], dictionary):
