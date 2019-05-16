72440f00caa954b515a2be0fe9eaebdad038cc12
Bryan Hanner <bryhan@cs.washington.edu>
2019-05-15 23:39:26 -0700

    makes all pair creation random

--- src/create_pairs.py
+++ src/create_pairs.py
@@ -22,62 +22,19 @@ def main():
         id = paper['id']
         text[id] = paper['title'] + ' ' + paper['paperAbstract']
         outCitations[id] = paper['outCitations']
-        if id == "78a4d1f70807f8a8b11a82636e8132c655d991cb" or id == "f80c1a392e20a35633a15d718a24ea6f54d2c58a":
-            print(text[id])
 
-    # counts numbers for train/dev/test split
-    true_citation_count = 0
-    one_hop_count = 0
-    for paper1, text1 in tqdm(text.items()):
-        for paper2 in outCitations[paper1]:
-            if paper2 in text.keys():
-                true_citation_count += 1
-    for paper1, text1 in tqdm(text.items()):
-        seen = set()
-        for paper2 in outCitations[paper1]:
-            if paper2 in text.keys():
-                for paper3 in outCitations[paper2]:
-                    if paper3 in text.keys() and paper3 not in seen and paper3 not in outCitations[paper1]:
-                        seen.add(paper3)
-                        one_hop_count += 1
     true_citation_count = 15000
     one_hop_count = 7500
     negative_examples = 7500
 
-    # cited pairs
-    processed = 0
-    first_paper = ""
-    for paper1, text1 in tqdm(text.items()):
-        for paper2 in outCitations[paper1][:2]:
-            if processed == 15:
-                first_paper = paper1
-            if paper2 in text.keys() and processed < true_citation_count:
-                if processed < int(0.8 * true_citation_count):
-                    out = train
-                elif processed >= int(0.8 * true_citation_count) and processed < int(0.9 * true_citation_count):
-                    out = dev
-                else:
-                    out = test
-                processed += 1
-                result = {}
-                result["query_paper"] = text1
-                result["candidate_paper"] = text[paper2]
-                result["relevance"] = "1"
-                if paper1 == first_paper:
-                    print(result)
-                out.write(json.dumps(result) + '\n')
-
     # random cited pairs
-    """
     processed = 0
     ids = list(text.keys())
-    for paper1, text1 in tqdm(text.items()):
-        count = 0
-        while count < 1:
-            paper2 = random.choice(ids)
-            if processed >= true_citation_count:
-                break
-            if paper2 in outCitations[paper1] and not paper1 == paper2:
+    while processed < true_citation_count:
+        paper1 = random.choice(ids)
+        if len(outCitations[paper1]) != 0:
+            paper2 = random.choice(outCitations[paper1])
+            if paper2 in text.keys():
                 if processed < int(0.8 * true_citation_count):
                     out = train
                 elif processed >= int(0.8 * true_citation_count) and processed < int(0.9 * true_citation_count):
@@ -85,50 +42,45 @@ def main():
                 else:
                     out = test
                 processed += 1
-                count += 1
                 result = {}
-                result["query_paper"] = text1
+                result["query_paper"] = text[paper1]
                 result["candidate_paper"] = text[paper2]
                 result["relevance"] = "1"
-                if paper1 == first_paper:
-                    print(result)
                 out.write(json.dumps(result) + '\n')
-    """
+    print(processed)
 
     processed = 0
 
     # one-hop pairs
-    for paper1, text1 in tqdm(text.items()):
-        seen = set()
-        for paper2 in outCitations[paper1]:
+    while processed < one_hop_count:
+        paper1 = random.choice(ids)
+        if len(outCitations[paper1]) != 0:
+            paper2 = random.choice(outCitations[paper1])
             if paper2 in text.keys():
-                for paper3 in outCitations[paper2]:
-                    if paper3 in text.keys() and paper3 not in seen and paper3 not in outCitations[paper1] and processed < one_hop_count:
+                if len(outCitations[paper2]) != 0:
+                    paper3 = random.choice(outCitations[paper2])
+                    if paper3 in text.keys() and paper3 not in outCitations[paper1]:
                         if processed < int(0.8 * one_hop_count):
                             out = train
                         elif processed >= int(0.8 * one_hop_count) and processed < int(0.9 * one_hop_count):
                             out = dev
                         else:
                             out = test
-                        seen.add(paper3)
                         processed += 1
                         result = {}
-                        result["query_paper"] = text1
+                        result["query_paper"] = text[paper1]
                         result["candidate_paper"] = text[paper3]
                         result["relevance"] = "0"
-                        if paper1 == first_paper:
-                            print(result)
                         out.write(json.dumps(result) + '\n')
+    print(processed)
 
     # random negatives
     processed = 0
-    ids = list(text.keys())
-    for paper1, text1 in tqdm(text.items()):
+    while processed < negative_examples:
+        paper1 = random.choice(ids)
         count = 0
         while count < 1:
             paper2 = random.choice(ids)
-            if processed >= negative_examples:
-                break
             if paper2 not in outCitations[paper1] and not paper1 == paper2:
                 if processed < int(0.8 * negative_examples):
                     out = train
@@ -139,12 +91,11 @@ def main():
                 processed += 1
                 count += 1
                 result = {}
-                result["query_paper"] = text1
+                result["query_paper"] = text[paper1]
                 result["candidate_paper"] = text[paper2]
                 result["relevance"] = "0"
-                if paper1 == first_paper:
-                    print(result)
                 out.write(json.dumps(result) + '\n')
+    print(processed)
 
 
 if __name__ == '__main__':
