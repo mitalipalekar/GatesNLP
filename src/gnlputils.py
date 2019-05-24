82bcb053635baf7ef4fff7c59a1a258a69e1e6dc
Swojit Mohapatra <neelmohapatra@gmail.com>
2019-05-24 10:59:35 -0700

    Only have finite values for abstracts tqdm added for vec generation gnlputils style fixes

--- src/gnlputils.py
+++ src/gnlputils.py
@@ -17,12 +17,12 @@ def get_from_rankings(rankings, dictionary):
 def split_data(data, dev_start: float, test_start: float, is_test: bool):
     train, dev, test = split_all_data(data, dev_start, test_start)
     if is_test:
-        return (train, test)
+        return train, test
     else:
-        return (train, dev)
+        return train, dev
+
 
 def split_all_data(data, dev_start: float, test_start: float):
     return (data[:int(dev_start * len(data))],
             data[int(dev_start * len(data)): int(test_start * len(data))],
             data[int(test_start * len(data)):])
-
